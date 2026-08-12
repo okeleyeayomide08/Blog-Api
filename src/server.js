2d11ea36cd5748709ec22f495ddf4eb362e31ed8
Okeleye Ayomide <okeleyeayomide08@gmail.com>
2026-08-12 06:30:23 +0100

    feat: add CORS support to the API and update package dependencies

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import { successMessage } from "./utils/apiResponse.js";
 import { connectDB } from "./config/database.js";
 import blogRoute from "./routes/blogRoutes.js";
@@ -8,6 +9,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/blogs", blogRoute);
 
